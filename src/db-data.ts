d33c8595746b9aa77cfffc87268723bbe775353d
jovanovska-a <anastasijajovanovska25@gmail.com>
2024-01-25 12:09:02 +0100

    domashna angular

---
+++ src/db-data.ts
@@ -0,0 +1,31 @@
+
+
+export const DRIVERS: any = [
+
+    //undefined,
+    {
+        id: 1,
+        name: "Charles Leclerc",
+        iconUrl:'https://monaco-addict.com/wp-content/uploads/2023/03/CHARLES-LECLERC-MONACOS-GOLDEN-BOY.webp',
+        team:"Dragons",
+        category:'ADVANCED',
+        pointers: 410
+    },
+    {
+        id: 2,
+        name: "Max Verstappen",
+        iconUrl:'https://img.redbull.com/images/c_limit,w_1500,h_1000,f_auto,q_auto/redbullcom/2023/2/22/cp88re7ulru1tqy0oidu/max-verstappen-red-bull-racing-kit-launch',
+        team:"Dragons",
+        category:'BEGINER',
+        pointers: 310
+    },
+    {
+        id: 3,
+        name: "Lewis Hamilton",
+        iconUrl:'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Lewis_Hamilton_2022_S%C3%A3o_Paulo_Grand_Prix_%2852498120773%29_%28cropped%29.jpg/640px-Lewis_Hamilton_2022_S%C3%A3o_Paulo_Grand_Prix_%2852498120773%29_%28cropped%29.jpg',
+        team:"Dragons",
+        category:'ADVANCED',
+        pointers: 210
+    },
+
+];
